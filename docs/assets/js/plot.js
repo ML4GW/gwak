a9cf8fa0638bff4dd25639da20b300a6339b3f9b
Katya Govorkova <ekaterina.govorkova@cern.ch>
2024-02-22 17:17:35 +0100

    fix old FAR axis

--- docs/assets/js/plot.js
+++ docs/assets/js/plot.js
@@ -1,3 +1,36 @@
+let go_fetch_old=(filename, folder, title)=>{
+fetch(filename).then((res) => res.json()).then((data) => {
+
+const layout = {
+        title: title,
+        width: 1100,
+        height: 650,
+        hovermode: 'closest',
+        xaxis: {
+            title: "Date"
+        },
+        yaxis: {
+            title: "FAR",
+            tickvals: [-1.617, -2.225, -2.985, -3.091],
+            ticktext: ['1/2 days', '1/week', '1/month', '1/year']        }
+    };
+
+    Plotly.newPlot('plot', data, layout);
+
+    document.getElementById('plot').on('plotly_click', function(data){
+
+        var pts = '';
+        for(var i=0; i < data.points.length; i++){
+            pts = 'x = '+data.points[i].x +'\ny = '+
+                data.points[i].y.toPrecision(4) + '\n\n';
+        }
+
+        show_image(`${folder}/${data.points[0].data.file[data.points[0].pointNumber]}`);
+
+    });
+});
+}
+
 let go_fetch=(filename, folder, title)=>{
 fetch(filename).then((res) => res.json()).then((data) => {
 
@@ -36,7 +69,7 @@ const layout = {
 
 document.getElementsByClassName("button")[0].addEventListener('click', function(){
     document.getElementsByClassName("button")[0].innerHTML = "O3a old"
-    go_fetch('all_O3a_spectrogram_old.json', 'all_O3a_spectrogram_old', 'O3a GWAK Detections before Heuristics')
+    go_fetch_old('all_O3a_spectrogram_old.json', 'all_O3a_spectrogram_old', 'O3a GWAK Detections before Heuristics')
 })
 document.getElementsByClassName("button")[1].addEventListener('click', function(){
     document.getElementsByClassName("button")[1].innerHTML = "O3a new pearson"
@@ -58,7 +91,7 @@ document.getElementsByClassName("button")[5].addEventListener('click', function(
     document.getElementsByClassName("button")[5].innerHTML = "Burst O3b training"
     go_fetch('burst_trainingO3b.json', 'burst_trainingO3b', 'Burst GWAK Detections O3b training')
 })
-document.getElementsByClassName("button")[0].innerHTML = "O3a analysis"
+document.getElementsByClassName("button")[2].innerHTML = "O3a analysis"
 go_fetch('all_O3a_spectrogram.json', 'all_O3a_spectrogram', 'O3a GWAK Detections no Pearson but with Heuristics')
 
 
