Deployment bug fix, highpass, normalization, and SNR calibration.

gwak/deploy/deploy/triton_inj_excution.py
@@ -20,56 +20,73 @@ EXTREME_CCSN = [
 ]
 
 def run_infer(
+    job_dir: Path,
     triton_server_ip: str,
     gwak_streamer: str,
     sequence_id: int,
     strain_file: Union[str, Path],
     data_format: str,
     shifts: list=[0.0, 1.0],
-    batch_size:int=1,
+    psd_length: float=64,
+    # batch_size:int=1,
     stride_batch_size:int=256,
     ifos:list=["H1", "L1"],
     kernel_size:int=2048,
-    sample_rate:int=2048,
+    sample_rate:int=4096,
     inference_sampling_rate:float=1,
     **kwargs
 ):
-    
-    sequence_kernel_size = kernel_size # Temporal setup to get over config setting
+
+
+    gwak_logger(job_dir / "log.log")
+    result_dir = job_dir.resolve().parents[1]
+    saving_dir =  result_dir / "inference_INJ_result"
+    saving_dir.mkdir(parents=True, exist_ok=True)
+
+
+    sequence = Sequence(
+        fname=strain_file,
+        data_format=data_format,
+        shifts=shifts,
+        psd_length=psd_length,
+        stride_batch_size=stride_batch_size,
+        ifos=ifos,
+        kernel_size=kernel_size,
+        sample_rate=sample_rate,
+        inference_sampling_rate=inference_sampling_rate,
+        inj_type=None,
+    )
+
+    client = InferenceClient(
+        f"{triton_server_ip}:8001", 
+        gwak_streamer,
+        callback=sequence,
+
+    )
+
+    # sequence_kernel_size = kernel_size # Temporal setup to get over config setting
     dis_grid_count = 4
     one_side_pad_length = 0.5
     kernel_length = 0.5
     kernel_size = int(kernel_length * sample_rate)
-    os.getenv # Use this to get ccsn config file. 
-    inj_type=None # Set injection type manully
-    client = InferenceClient(f"{triton_server_ip}:8001", gwak_streamer)
+    # os.getenv # Use this to get ccsn config file. 
+    # inj_type=None # Set injection type manully
     print(os.getenv("CCSN_FILE"))
-    
+
+    signals_dict = load_h5_as_dict(
+        chosen_signals="/home/hongyin.chen/anti_gravity/gwak/gwak/deploy/deploy/config/ccsn.yaml",
+        source_file="/home/hongyin.chen/Data/3DCCSN_PREMIERE/Resampled"
+    )
+
+    # No additional time shift
+    projector = CCSN_Waveform_Projector(
+        ifos=ifos,
+        sample_rate=sample_rate,
+        sample_duration=3, 
+    )
+
+
     with client:
-
-        sequence = Sequence(
-            fname=strain_file,
-            data_format=data_format,
-            shifts=shifts,
-            batch_size=batch_size,
-            ifos=ifos,
-            kernel_size=sequence_kernel_size,
-            sample_rate=sample_rate,
-            inference_sampling_rate=inference_sampling_rate,
-            inj_type=None,
-        )
-
-        signals_dict = load_h5_as_dict(
-            chosen_signals="/home/hongyin.chen/anti_gravity/gwak/gwak/deploy/deploy/config/ccsn.yaml",
-            source_file="/home/hongyin.chen/Data/3DCCSN_PREMIERE/Resampled_2048"
-        )
-
-        # No additional time shift
-        projector = CCSN_Waveform_Projector(
-            ifos=ifos,
-            sample_rate=sample_rate,
-            sample_duration=3, 
-        )
 
         for key_idx, key in enumerate(signals_dict.keys()):
             logging.info(f"Running {key} analysis")
@@ -92,22 +109,24 @@ def run_infer(
                     padded_ccsne = projector(
                         time = signals_dict[key][0],
                         quad_moment = signals_dict[key][1],
-                        ori_theta=np.zeros(stride_batch_size),
-                        ori_phi=np.zeros(stride_batch_size),
-                        dec=np.zeros(stride_batch_size),
-                        psi=np.zeros(stride_batch_size),
-                        phi=np.zeros(stride_batch_size),
+                        ori_theta=np.zeros(int(stride_batch_size/inference_sampling_rate)),
+                        ori_phi=np.zeros(int(stride_batch_size/inference_sampling_rate)),
+                        dec=np.zeros(int(stride_batch_size/inference_sampling_rate)),
+                        psi=np.zeros(int(stride_batch_size/inference_sampling_rate)),
+                        phi=np.zeros(int(stride_batch_size/inference_sampling_rate)),
                     )
 
                     # Sample start from the middle of the padded CCSN since no additainl time shift applied
-                    ccsne = padded_ccsne[:,:,3072:3072 + int(kernel_length * sample_rate)] / dis
+                    ccsne = padded_ccsne[:,:,6144:6144 + int(kernel_length * sample_rate)] / dis
+                    with h5py.File("/home/hongyin.chen/gwak_ccsn.h5", "w") as h:
+                        h.create_dataset(f"{dis}", data=padded_ccsne)
 
                     # Can be replace by reshape ccsne and add the two array.
                     for inj_idx, ccsn in enumerate(ccsne):
 
                         inj_start = int((one_side_pad_length + inj_idx) * sample_rate)
                         inj_end = inj_start + kernel_size
-                        inj_state[:, inj_start: inj_end] += ccsn  
+                        inj_state[:, inj_start: inj_end] += ccsn * 10
 
                     
                     client.infer(
@@ -118,17 +137,26 @@ def run_infer(
                         sequence_end=sequence_end,
                     )
 
-                    result = client.get()
-                    while result is None:
-                        result = client.get()
-                    results.append(result[0])
+                    # result = client.get()
+                    # while result is None:
+                    #     result = client.get()
+                    # results.append(result[0])
                     
+                    if not i:
+                        while not sequence.started:
+                            client.get()
+                            time.sleep(1e-2)
+                # Retrieve response from the triton server. 
+                result = client.get()
+                while result is None:
+                    result = client.get()
+                results.append(result[0])
                     # One sequence ends leave the sequence loop
                 # For each Distance, of each CCSN template we save one file
                 
                 results = np.stack(results)
                 key_name = f"{key.replace('/', '_')}/DIS_{dis:05.1f}"
-                saving_dir = Path(f"../../inference_INJ_result/{key.replace('/', '_')}")
+                saving_dir = saving_dir / f"{key.replace('/', '_')}"
                 saving_dir.mkdir(parents=True, exist_ok=True)
                 result_file = saving_dir / f"Inj_sequence_{sequence_id}.h5"
                 logging.info(f"Collecting result to {result_file.resolve()}")

gwak/train/dataloader.py
@@ -574,11 +574,15 @@ class SignalDataloader(GwakBaseDataloader):
 
         psd_resample_size = 1+injected.shape[-1]//2 if injected.shape[-1] % 2 == 0 else (injected.shape[-1]+1)//2
         psds_resampled = F.interpolate(psds.double(), size=psd_resample_size, mode='linear', align_corners=False)
-        snrs = compute_ifo_snr(injected.double(), psds_resampled, self.sample_rate)
 
-        # compute network SNR 
-        snrs = snrs**2
-        snrs = torch.sum(snrs, dim=-1)  ** 0.5
+        snrs = None
+        if waveforms is not None: 
+            
+            snrs = compute_ifo_snr(waveforms, psds_resampled, self.sample_rate)
+
+            # compute network SNR 
+            snrs = snrs**2
+            snrs = torch.sum(snrs, dim=-1)  ** 0.5
 
         # normalize the input data
         stds = torch.std(whitened, dim=-1, keepdim=True)